9318fd3e006b2c64b833db7ffcab48621f32562b
Peyman Daei Rezaei <88220773+Peyman2012@users.noreply.github.com>
2023-07-05 01:18:53 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+a = 2
+ent = int(ent)
+rand = random.randint(1,100)
+print(rand)
+def game_random(rand):
+    a = True
+    while a :
+        if ent=="b":
+            if rand <=0 and
+            print("mine is larger")
+        elif ent<rand:
+            print("mine is smaller")
+        elif ent == rand:
+            print("you are roke")
+        if
+        rand = random.randint(1, 100)
+        print(rand)
+        ent = input("Enter your letter : ")
+while a>0:
+    game_random(ent)
+
+    write = input("are you ready(y,n) : ")
+    if write == "n":
+        break
+    elif write=="y":
+        a += 1
+    rand = random.randint(1, 100)
+
